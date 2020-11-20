91ef043accbb07fbed37ac61352a2e16a339b92b
XiangyuG <xg673@nyu.edu>
2020-11-20 02:23:12 -0500

    Formalize ILP_solver program

--- ILP_solver.py
+++ ILP_solver.py
@@ -1,39 +1,50 @@
 import z3
+import sys
 
-z3_slv = z3.Solver()
-# Input file containing variables and constraints
-'''
-Format input:
----------------
-Variables:
-x
-y
-z
-Constraints:
-x > 10
-y == x + 2
-z <= 20
----------------
-'''
-filename = "/tmp/ILP.txt"
-f = open(filename,"r")
-
-
-while 1:
-    l = f.readline()
-    if l == "Variables:\n":
-        while 1:
-            l = f.readline()
-            if l == "Constraints:\n":
-                break
-            # remove /n from this string
-            l = l[:-1]
-            # Ref website:https://www.programiz.com/python-programming/methods/built-in/exec
-            exec(l+'=z3.Int(\'%s\')'%l)
-    elif l:           
-        # Ref website: 
-        z3_slv.add(eval(l))
+def main(argv):
+    """main program."""
+    if len(argv) >= 3:
+        print("Usage: python3 " + argv[0] + " <ILP filename>(optional)")
+    z3_slv = z3.Solver()
+    # Input file containing variables and constraints
+    '''
+    Format input:
+    ---------------
+    Variables:
+    x
+    y
+    z
+    Constraints:
+    x > 10
+    y == x + 2
+    z <= 20
+    ---------------
+    '''
+    if len(argv) == 2:
+        filename = argv[1]
     else:
-        break
-print(z3_slv.check())
-print(z3_slv.model())
+        filename = "/tmp/ILP.txt"
+    f = open(filename,"r")
+    
+    
+    while 1:
+        l = f.readline()
+        if l == "Variables:\n":
+            while 1:
+                l = f.readline()
+                if l == "Constraints:\n":
+                    break
+                # remove /n from this string
+                l = l[:-1]
+                # Ref website:https://www.programiz.com/python-programming/methods/built-in/exec
+                exec(l+'=z3.Int(\'%s\')'%l)
+        elif l:           
+            # Ref website: 
+            z3_slv.add(eval(l))
+        else:
+            break
+    print(z3_slv.check())
+    print(z3_slv.model())
+
+if __name__ == "__main__":
+    main(sys.argv)
