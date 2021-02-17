a0a81c9b6483c28c81a6f2247b617aa19cc6b7c6
XiangyuG <xg673@nyu.edu>
2021-02-17 08:55:24 -0500

    Add objective function into ILP solver

--- ILP_solver.py
+++ ILP_solver.py
@@ -5,7 +5,7 @@ def main(argv):
     """main program."""
     if len(argv) >= 3:
         print("Usage: python3 " + argv[0] + " <ILP filename>(optional)")
-    z3_slv = z3.Solver()
+    z3_slv = z3.Optimize()
     # Input file containing variables and constraints
     '''
     Format input:
@@ -20,13 +20,15 @@ def main(argv):
     z <= 20
     ---------------
     '''
+    # cost is our objective func
+    cost = z3.Int("cost")
     if len(argv) == 2:
         filename = argv[1]
     else:
         filename = "/tmp/ILP.txt"
     f = open(filename,"r")
     
-    
+    var_list = [] 
     while 1:
         l = f.readline()
         if l == "Variables:\n":
@@ -36,6 +38,7 @@ def main(argv):
                     break
                 # remove /n from this string
                 l = l[:-1]
+                var_list.append(l)
                 # Ref website:https://www.programiz.com/python-programming/methods/built-in/exec
                 exec(l+'=z3.Int(\'%s\')'%l)
         elif l:           
@@ -43,7 +46,11 @@ def main(argv):
             z3_slv.add(eval(l))
         else:
             break
+    for v in var_list:
+        z3_slv.add(eval("cost >= " + v))
+    h = z3_slv.minimize(cost)
     print(z3_slv.check())
+    print(z3_slv.lower(h))
     print(z3_slv.model())
 
 if __name__ == "__main__":
