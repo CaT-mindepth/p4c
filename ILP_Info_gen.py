1b1da944f17314176cff5d1d540a765ca5dead0d
XiangyuG <xg673@nyu.edu>
2021-02-24 21:57:43 -0500

    Update ILP_Info_gen to support new format

--- ILP_Info_gen.py
+++ ILP_Info_gen.py
@@ -2,7 +2,7 @@ import sys
 import random
 
 def main(argv):
-    # TODO: check argv length
+    # check argv length
     """main program"""
     """Format: python3 ILP_Info_gen.py <Table num>"""
     if len(argv) != 2:
@@ -27,19 +27,26 @@ def main(argv):
             output_str += t1 + " has Action dependency relationship with " + t2 + "\n"
 
     output_str += "Action Info:\n"
-    # length is from [1, 10]
-    # width is from [1,5]
+    # alu_num is from [1, 10]
+    # posibility that a pair has dependency is 0.1 
     # TODO: make them into parameter
+    # Format: T2:5;(1,2);(1,3);(2,4);(3,5)
     for i in range(1, table_num + 1):
-        stage = random.randint(1, 10)
         output_str += "T" + str(i) + ":"
-        for j in range(stage):
-            alu_num = random.randint(1,5)
-            output_str += str(alu_num)
-            if j == stage - 1:
-                output_str += "\n"
-            else:
-                output_str += ","
+        alu_num = random.randint(1,10)
+        output_str += str(alu_num) + ";"
+        add_flag = 0
+        for j in range(1, alu_num + 1):
+            for k in range(j + 1, alu_num + 1):
+                alu_dep_rand = random.randint(0,9)
+                if alu_dep_rand == 0:
+                    output_str += "(" + str(j) + "," + str(k) + ");"
+                    add_flag = 1
+        if output_str[-1] == ";" and add_flag == 1:
+            output_str = output_str[:-1]
+            output_str += "\n"
+        elif i != table_num:
+            output_str += "\n"
     print(output_str)
 
 if __name__ == "__main__":
